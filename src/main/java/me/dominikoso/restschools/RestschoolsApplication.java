f6e54fc2fb577b80bf03e8491e9d00ad168552b9
Dominik K <rodzinka.kostkow@gmail.com>
2019-10-28 18:42:55 +0100

    Spring Initialization

---
+++ src/main/java/me/dominikoso/restschools/RestschoolsApplication.java
@@ -0,0 +1,13 @@
+package me.dominikoso.restschools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestschoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestschoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dominikoso/restschools/RestschoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dominikoso.restschools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestschoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
