c469c85825b6b527b3d6c45b72756ef3a8260fba
dominikoso <mcdominikoso@gmail.com>
2019-10-29 20:50:59 +0100

    Added field filtering

--- src/main/java/me/dominikoso/restschools/controller/SchoolController.java
+++ src/main/java/me/dominikoso/restschools/controller/SchoolController.java
@@ -2,24 +2,59 @@ package me.dominikoso.restschools.controller;
 
 import me.dominikoso.restschools.model.School;
 import me.dominikoso.restschools.repository.SchoolRepository;
+import me.dominikoso.restschools.tools.ResponseControllersTools;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/school")
+@RequestMapping(value = "/school", produces = "application/json; charset=utf-8")
 public class SchoolController {
 
     @Autowired
     private SchoolRepository schoolRepository;
 
+    private ResponseControllersTools controllersTools = new ResponseControllersTools();
+
     @RequestMapping(method = RequestMethod.GET)
-    public List<School> getAll() {return schoolRepository.findAll();}
+    public ResponseEntity getAll(@RequestParam(value = "fields", required = false) String fields) {
+        return ResponseEntity.ok(getSchools(fields));
+    }
+
+    private Object getSchools(String fields) {
+        List<School> schools = schoolRepository.findAll();
+        return controllersTools.parsedSchools(schools, fields);
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "/city/{city}")
-    public List<School> getAllByCity(@PathVariable(name = "city") String city) {return schoolRepository.findAllByCity(city);}
+    public ResponseEntity getAllByCity(@RequestParam(value = "fields", required = false) String fields,
+                                     @PathVariable(name = "city") String city) {
+        return ResponseEntity.ok(getSchoolsByCity(fields, city));
+    }
+
+    private Object getSchoolsByCity(String fields, String city) {
+        List<School> schools = schoolRepository.findAllByCity(city);
+        return controllersTools.parsedSchools(schools, fields);
+    }
 
     @RequestMapping(method = RequestMethod.GET, value = "/type/{type}")
-    public List<School> getAllByType(@PathVariable(name = "type") String type) {return schoolRepository.findAllByType(type);}
+    public ResponseEntity getAllByType(@RequestParam(value = "fields", required = false) String fields,
+                                     @PathVariable(name = "type") String type) {
+        return ResponseEntity.ok(getSchoolsByTypes(fields, type));
+    }
+
+    private Object getSchoolsByTypes(String fields, String type) {
+        List<School> schools = schoolRepository.findAllByType(type);
+        return controllersTools.parsedSchools(schools, fields);
+    }
+
+
+    @PostMapping
+    public ResponseEntity blockPost(){
+        return  ResponseEntity.status(HttpStatus.FORBIDDEN).body("We don't do that here");
+    }
+
 }
