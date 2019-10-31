49254dcad5c20b6736aed2d3b8435e9764457e9b
dominikoso <mcdominikoso@gmail.com>
2019-10-31 21:04:14 +0100

    Added `WebController` for rendering home page

---
+++ src/main/java/me/dominikoso/restschools/controller/WebController.java
@@ -0,0 +1,24 @@
+package me.dominikoso.restschools.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WebController {
+
+    private List<String> endpoints = Arrays.asList("id", "city", "type", "wojewodztwo", "powiat", "name");
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePageLoader(Model model){
+        model.addAttribute("endpoints", endpoints);
+
+        return "home";
+    }
+
+}
