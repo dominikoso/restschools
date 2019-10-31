7bd20daae9199d33cfa6b5e858da0572f261e267
dominikoso <mcdominikoso@gmail.com>
2019-10-31 21:03:05 +0100

    Rename `ResponseControllersTools` to `SchoolControllersTools`

--- src/main/java/me/dominikoso/restschools/tools/SchoolControllersTools.java
+++ src/main/java/me/dominikoso/restschools/tools/SchoolControllersTools.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ResponseControllersTools {
+public class SchoolControllersTools {
 
     public Object parsedSchool(School school, String fields) {
         if (fields == null || fields.isEmpty()) {
