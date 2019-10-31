50429f7c0a289faa5cebeaa3ecb517a6d3fb1991
dominikoso <mcdominikoso@gmail.com>
2019-10-31 21:30:55 +0100

    Added Banner, Template correction

--- src/main/java/me/dominikoso/restschools/controller/WebController.java
+++ src/main/java/me/dominikoso/restschools/controller/WebController.java
@@ -6,17 +6,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class WebController {
 
-    private List<String> endpoints = Arrays.asList("id", "city", "type", "wojewodztwo", "powiat", "name");
+    private List<String> endpoints = Arrays.asList("", "id", "city", "type", "wojewodztwo", "powiat", "name");
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String homePageLoader(Model model){
+    public String homePageLoader(Model model, HttpServletRequest request){
         model.addAttribute("endpoints", endpoints);
+        model.addAttribute("fullUrl", request.getRequestURL().toString());
 
         return "home";
     }
