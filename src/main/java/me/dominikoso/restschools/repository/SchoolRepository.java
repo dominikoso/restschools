c1151bc933c84def083c3e7ff35cd3d05b73a0dd
dominikoso <mcdominikoso@gmail.com>
2019-11-01 18:39:19 +0100

    Added caching for repository
    Added temporary may be removed in future

--- src/main/java/me/dominikoso/restschools/repository/SchoolRepository.java
+++ src/main/java/me/dominikoso/restschools/repository/SchoolRepository.java
@@ -1,6 +1,7 @@
 package me.dominikoso.restschools.repository;
 
 import me.dominikoso.restschools.model.School;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -16,35 +17,42 @@ public interface SchoolRepository extends CrudRepository<School, Long> {
     /**
      * Function that fetch all entries from schools table.
      */
+    @Cacheable("all")
     List<School> findAll();
     /**
      * Function that fetch one entry from schools table by id.
      * @param id database id
      */
+    @Cacheable("id")
     School findBySchoolId(Long id);
     /**
      * Function that fetch all entries from schools table by location.
      * @param city city name
      */
+    @Cacheable("city")
     List<School> findAllByCity(String city);
     /**
      * Function that fetch all entries from schools table by school type.
      * @param type school type
      */
+    @Cacheable("type")
     List<School> findAllByType(String type);
     /**
      * Function that fetch all entries from schools table by location.
      * @param wojewodztwo wojewodztwo name
      */
+    @Cacheable("wojewodztwo")
     List<School> findAllByWojewodztwo(String wojewodztwo);
     /**
      * Function that fetch all entries from schools table by location.
      * @param powiat powiat name
      */
+    @Cacheable("powiat")
     List<School> findAllByPowiat(String powiat);
     /**
      * Function that fetch all entries from schools table by name likability.
      * @param name part of whole name of school
      */
+    @Cacheable("name")
     List<School> findAllBySchoolFullNameContaining(String name);
 }
